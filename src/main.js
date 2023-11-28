720b9723903e4cba584fe75035178d5421511e3f
Danilo Velasquez <cvelasquez@lifefile.net>
2023-11-27 21:16:55 -0500

    test deploy

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 
-import { createApp, markRaw } from './vue'
-import { createPinia } from './pinia'
-import createPersistedState from './pinia-plugin-persistedstate'
-import './bootstrap/dist/css/bootstrap.min.css'
-import './@fortawesome/fontawesome-free/css/all.min.css'
-import './bootstrap/dist/js/bootstrap.bundle'
-import App from '/App.vue'
-import router from '/router'
-import axios from './axios'
+import { createApp, markRaw } from '../vue'
+import { createPinia } from '../pinia'
+import createPersistedState from '../pinia-plugin-persistedstate'
+import '../bootstrap/dist/css/bootstrap.min.css'
+import '../@fortawesome/fontawesome-free/css/all.min.css'
+import '../bootstrap/dist/js/bootstrap.bundle'
+import App from './App.vue'
+import router from './router'
+import axios from '../axios'
 
 window.axios = axios
 window.axios.defaults.baseURL = 'http://localhost:8000'
